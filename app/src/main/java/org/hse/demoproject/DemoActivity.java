6b2c5a34fdb3af9159125556f91775501f0f34fa
Eugene <oneevgexaone@gmail.com>
2024-02-02 20:14:37 +0500

    Initial Commit

--- app/src/main/java/org/hse/demoproject/DemoActivity.java
+++ app/src/main/java/org/hse/demoproject/DemoActivity.java
@@ -2,6 +2,7 @@ package org.hse.demoproject;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -13,64 +14,70 @@ import java.util.List;
 import java.util.Locale;
 
 public class DemoActivity extends AppCompatActivity {
-
     private TextView result;
     private EditText number;
-
-    private List<Integer> GetList(){
-        String numberVal = number.getText().toString();
-        if (numberVal.isEmpty()){
-            numberVal = "0";
-        }
-        int count = Integer.parseInt(numberVal);
-
-        if (count < 1 || count > 50) {
-
-            Toast.makeText(this, "Введите число в диапазоне от 1 до 50", Toast.LENGTH_SHORT).show();
-            return new ArrayList<>();
-        }
-
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < count; i++){
-            list.add(i+1);
-        }
-        return list;
-    }
-
-    private void clickButton1(){
-
-        if (!GetList().isEmpty()){
-            int sum = GetList().stream().mapToInt(Integer::intValue).sum();
-            result.setText(String.format(Locale.US,"Result: %d", sum));
-        }
-        else{
-            result.setText(String.format(Locale.US,"Result: %d", 0));
-        }
-    }
-
-    private void clickButton2(){
-        if (!GetList().isEmpty()){
-            long product = GetList().stream().filter(num -> num % 2 == 0).mapToLong(Integer::intValue).reduce(1, (a, b) -> a * b);
-            result.setText(String.format(Locale.US,"Result: %d", product));
-        }
-        else{
-            result.setText(String.format(Locale.US,"Result: %d", 0));
-        }
-    }
+    private List<Integer> numberList;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_demo);
 
         number = findViewById(R.id.number);
-
-        View button1 = findViewById(R.id.button1);
-        View button2 = findViewById(R.id.button2);
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
         result = findViewById(R.id.result);
 
-        button1.setOnClickListener(v -> clickButton1());
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickButton(1);
+            }
+        });
 
-        button2.setOnClickListener(v -> clickButton2());
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickButton(2);
+            }
+        });
+    }
+    private void clickButton(int buttonNumber) {
+        String numberVal = number.getText().toString();
+        if (numberVal.isEmpty()) {
+            numberVal = "0";
+        }
+
+        try {
+            long count = Long.parseLong(numberVal);
+
+            if (count < 1 || count > 50) {
+                Toast.makeText(this, getString(R.string.range_error), Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            numberList = new ArrayList<>();
+            for (int i = 0; i < count; i++) {
+                numberList.add(i + 1);
+            }
+
+            if (buttonNumber == 1) {
+                int sum = 0;
+                for (int num : numberList) {
+                    sum += num;
+                }
+                result.setText(String.format(Locale.US, "Result: %d", sum));
+            } else if (buttonNumber == 2) {
+                long product = 1;
+                for (int num : numberList) {
+                    if (num % 2 == 0) {
+                        product *= num;
+                    }
+                }
+                result.setText(String.format(Locale.US, "Result: %d", product));
+            }
+        } catch (NumberFormatException e) {
+            Toast.makeText(this, getString(R.string.range_error), Toast.LENGTH_SHORT).show();
+        }
     }
 }
